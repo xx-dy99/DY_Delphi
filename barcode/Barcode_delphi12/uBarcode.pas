16775312d2e6a675ae873df14de1bb736d570e2b
xx-dy99 <dong991016@naver.com>
2025-03-21 11:00:18 +0900

    Barcode - delphi12 code change fin.

--- barcode/Barcode_delphi12/uBarcode.pas
+++ barcode/Barcode_delphi12/uBarcode.pas
@@ -18,6 +18,7 @@ type
     procedure ApdComPort1TriggerAvail(CP: TObject; Count: Word);
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure btnTriggerScanClick(Sender: TObject);
+
   private
     { Private declarations }
   public
@@ -26,7 +27,8 @@ type
 
 var
   Form1: TForm1;
-  PartialData: string = '';
+  //기존 PartialData: string = '';
+  PartialData: AnsiString = '';
 
 implementation
 
@@ -46,13 +48,13 @@ begin
     Exit;
   end;
 
-  ApdComPort1.TriggerLength := 1;
+  ApdComPort1.TriggerLength := 1; //바코드리더기가 데이터를 1바이트 받으면.
 
-  try
-    ApdComPort1.Open := True;
+  try //오류(예외)가 발생할수있는 코드블룩 시작 발생할경우 except블록으로 이동하여 오류를 처리.
+    ApdComPort1.Open := True; //바코드 데이터를 받을준비함.
     ShowMessage('포트가 열렸습니다.');
   except
-    on E: Exception do
+    on E: Exception do //예외(오류)를 잡아서 E라는 변수에 저장하고 오류메시지 확인.
       ShowMessage('포트 열기 실패:' + E.Message);
   end;
 end;
@@ -70,26 +72,41 @@ end;
 
 procedure TForm1.ApdComPort1TriggerAvail(CP: TObject; Count: Word);
 var
-  ReceivedData: array[0..255] of char;
-  DataStr: string;
+  //기존 ReceivedData: array[0..255] of char;
+  ReceivedData: array[0..255] of Byte;
+  DataStr: AnsiString;
+  I: Integer;//추가
 begin
   FillChar(ReceivedData, Sizeof(ReceivedData), 0);
   ApdComPort1.GetBlock(ReceivedData, Count);
-  DataStr := StrPas(ReceivedData);
+  // 기존 (제거)DataStr := StrPas(ReceivedData);
 
+  //추가 바이트 데이터를 AnsiString으로 변환
+  SetLength(DataStr, Count);
+  for I := 0 to Count - 1 do
+    DataStr[I + 1] := AnsiChar(ReceivedData[I]);
+
+  //<STX>감지 시 새로운 바코드 시작
   if Pos(#2, DataStr) > 0 then
     PartialData := '';
+
+  //데이터 누적 저장
   PartialData := PartialData + DataStr;
 
+  // <STX>, <CR>, <LF> 제거
   PartialData := StringReplace(PartialData, #2, '', [rfReplaceAll]);
   PartialData := StringReplace(PartialData, #13, '', [rfReplaceAll]);
   PartialData := StringReplace(PartialData, #10, '', [rfReplaceAll]);
 
+  // 너무 짧은 데이터 무시
   if Length(PartialData) < 8 then Exit;
 
   if (Pos(#13, DataStr) > 0) or (Pos(#10, DataStr) > 0) then
   begin
-    Memo1.Lines.Add(PartialData);
+    //기존 Memo1.Lines.Add(PartialData);
+    Memo1.Lines.Add(string(PartialData));//추가 AnsiString을 Unicode로변환후 Memo1에 추가
+    Memo1.SelStart := Length(Memo1.Text); //자동스크롤 이동
+    SendMessage(Memo1.Handle, WM_VSCROLL, SB_BOTTOM, 0); //추가스크롤 최하단 이동
     PartialData:= '';
   end;
 end;
@@ -103,12 +120,13 @@ begin
   end;
 end;
 
+//기존 팝업창을 닫고 다시 스캔클릭하고 반복하는 불편함이있음.
 procedure TForm1.btnTriggerScanClick(Sender: TObject);
 begin
   if ApdComPort1.Open then
   begin
     ApdComPort1.Output := #2;
-    ShowMessage('스캔명령을 전송했씁니다.');
+    ShowMessage('스캔명령을 전송했습니다.');
   end
   else
     ShowMessage('포트가 열려있지 않습니다.');
