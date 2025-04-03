3399623715b7f28e237246bb3bf210c3e05b7c46
xx-dy99 <dong991016@naver.com>
2025-04-03 11:48:36 +0900

    Motor delphi12 add Status Form

--- Motor/Motor_delphi12/uMotor.pas
+++ Motor/Motor_delphi12/uMotor.pas
@@ -30,6 +30,102 @@ type
     Panel1: TPanel;
     Label1: TLabel;
     Label2: TLabel;
+    btnRunRvs: TButton;
+    Panel3: TPanel;
+    Panel4: TPanel;
+    Panel5: TPanel;
+    Panel6: TPanel;
+    Panel7: TPanel;
+    Panel8: TPanel;
+    Panel9: TPanel;
+    Panel10: TPanel;
+    Panel11: TPanel;
+    GroupBox1: TGroupBox;
+    Panel2: TPanel;
+    Panel12: TPanel;
+    Panel13: TPanel;
+    Panel14: TPanel;
+    Panel15: TPanel;
+    Panel16: TPanel;
+    Panel17: TPanel;
+    Panel18: TPanel;
+    Panel19: TPanel;
+    Panel20: TPanel;
+    Panel21: TPanel;
+    Panel22: TPanel;
+    Panel23: TPanel;
+    Panel24: TPanel;
+    Panel25: TPanel;
+    Panel26: TPanel;
+    Panel27: TPanel;
+    Panel28: TPanel;
+    Panel47: TPanel;
+    Panel48: TPanel;
+    Panel49: TPanel;
+    Panel50: TPanel;
+    Panel51: TPanel;
+    Panel52: TPanel;
+    Panel53: TPanel;
+    Panel54: TPanel;
+    Panel55: TPanel;
+    Panel29: TPanel;
+    Panel30: TPanel;
+    Panel31: TPanel;
+    Panel32: TPanel;
+    Panel33: TPanel;
+    Panel34: TPanel;
+    Panel35: TPanel;
+    Panel36: TPanel;
+    Panel37: TPanel;
+    Panel41: TPanel;
+    Panel46: TPanel;
+    Panel56: TPanel;
+    Panel57: TPanel;
+    Panel58: TPanel;
+    Panel59: TPanel;
+    Panel60: TPanel;
+    Panel61: TPanel;
+    Panel62: TPanel;
+    Panel63: TPanel;
+    Panel64: TPanel;
+    Panel65: TPanel;
+    Panel66: TPanel;
+    Panel67: TPanel;
+    Panel68: TPanel;
+    Panel69: TPanel;
+    Panel70: TPanel;
+    Panel71: TPanel;
+    Panel72: TPanel;
+    Panel73: TPanel;
+    Panel74: TPanel;
+    Panel75: TPanel;
+    Panel76: TPanel;
+    Panel77: TPanel;
+    Panel78: TPanel;
+    Panel79: TPanel;
+    Panel80: TPanel;
+    Panel81: TPanel;
+    Panel82: TPanel;
+    Panel83: TPanel;
+    Panel84: TPanel;
+    Panel85: TPanel;
+    Panel86: TPanel;
+    Panel87: TPanel;
+    Panel88: TPanel;
+    Panel89: TPanel;
+    Panel90: TPanel;
+    Panel91: TPanel;
+    Panel92: TPanel;
+    Panel93: TPanel;
+    Panel94: TPanel;
+    Panel95: TPanel;
+    Panel96: TPanel;
+    Panel97: TPanel;
+    Panel98: TPanel;
+    Panel99: TPanel;
+    Panel100: TPanel;
+    GroupBox2: TGroupBox;
+    GroupBox3: TGroupBox;
     procedure SendModbusQuery(const Data: array of Byte);
     procedure btnConnectClick(Sender: TObject);
     procedure wSocketSessionConnected(Sender: TObject; ErrCode: Word);
@@ -47,6 +143,7 @@ type
     procedure btnStartZeroClick(Sender: TObject);
     procedure btnStopZeroClick(Sender: TObject);
     procedure btnEmergencyStopClick(Sender: TObject);
+    procedure btnRunRvsClick(Sender: TObject);
 
   private
     { Private declarations }
@@ -116,6 +213,7 @@ end;
 
 procedure TForm1.btnConnectClick(Sender: TObject);
 begin
+  pnlStatus.ParentBackground := False;
   pnlStatus.Color := clBlue;
   pnlStatus.Font.Color := clBlack;
   pnlStatus.Caption := '연결시도중...';
@@ -131,12 +229,14 @@ procedure TForm1.wSocketSessionConnected(Sender: TObject; ErrCode: Word);
 begin
   if ErrCode = 0 then
   begin
+    pnlStatus.ParentBackground := False;
     pnlStatus.Color := clLime;
     pnlStatus.Font.Color := clBlack;
     pnlStatus.Caption := '연결 완료';
   end
   else
   begin
+    pnlStatus.ParentBackground := False;
     pnlStatus.Color := clRed;
     pnlStatus.Font.Color := clBlack;
     pnlStatus.Caption := '연결 실패';
@@ -145,6 +245,7 @@ end;
 
 procedure TForm1.wSocketSessionClosed(Sender: TObject; ErrCode: Word);
 begin
+  pnlStatus.ParentBackground := False;
   pnlStatus.Color := clGray;
   pnlStatus.Font.Color := clWhite;
   pnlStatus.Caption := '연결 끊어짐';
@@ -154,6 +255,7 @@ procedure TForm1.btnDisconnectClick(Sender: TObject);
 begin
   wSocket.Close;
 
+  pnlStatus.ParentBackground := False;
   pnlStatus.Color := clGray;
   pnlStatus.Font.Color := clWhite;
   pnlStatus.Caption := '연결 해제됨';
@@ -334,6 +436,17 @@ begin
     $06,
     $00, $1E,
     $22, $01
+  ]);
+end;
+
+procedure TForm1.btnRunRvsClick(Sender: TObject);
+begin
+  SendModbusQuery
+  ([
+    $01,
+    $06,
+    $00, $1E,
+    $24, $01
   ]);
 end;
 
